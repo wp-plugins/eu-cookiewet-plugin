b0aa9498fa4848fae03909206b0294e1ef0df2b5
mijndert <mijndert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-12 13:35:08 +0000

    figured out how to update the version number
    git-svn-id: https://plugins.svn.wordpress.org/eu-cookiewet-plugin/trunk@611474 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- eu-cookiewet-plugin.php
+++ eu-cookiewet-plugin.php
@@ -3,9 +3,9 @@
 /*
 Plugin Name: EU Cookiewet Plugin (Nederlands)
 Plugin URI: http://github.com/mijndert
-Description: Een simpele plugin voor WordPress om je website te laten voldoen aan de cookiewet. De plugin zal een balk bovenaan de website plaatsen om de gebruiker laten weten dat er cookies gebruikt worden. Ook wordt er een pagina aangemaakt welke linkt naar een zelf in te vullen cookie policy.
+Description: Een simpele plugin voor WordPress om een boodschap op je website te plaatsen dat je cookies gebruikt.
 Author: Mijndert Stuij
-Version: 1.0
+Version: 1.2
 Author URI: http://mijndertstuij.nl/
 */
 
